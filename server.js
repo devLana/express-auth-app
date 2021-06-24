f23571bbdbcc74a8e6d2438dc0beb7fa0305c738
ade_lana <lanadvictor@yahoo.co.uk>
2021-06-24 18:11:42 +0100

    connecting route  handlers to server

--- server.js
+++ server.js
@@ -3,6 +3,13 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3030;
 
+const router = require("./routes");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use("/api", router);
+
 app.listen(port, () => {
   console.log(`server running on port ${port}`);
 });
