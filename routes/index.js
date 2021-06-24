4cdd4adec120208bffb287de4ab07b8e2798366b
ade_lana <lanadvictor@yahoo.co.uk>
2021-06-24 18:11:06 +0100

    set up sign up route handler

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 
+const signUp = require("../controllers/signUp");
 const signIn = require("../controllers/signIn");
 
 const router = express.Router();
 
+router.post("/signup", signUp);
 router.post("/signin", signIn);
 
 module.exports = router;
