afcdf38fd6ec9b92898f1bc816a648b64e268b7b
ade_lana <lanadvictor@yahoo.co.uk>
2021-06-26 17:58:07 +0100

    set up sig in route controller

--- controllers/signIn.js
+++ controllers/signIn.js
@@ -1,6 +1,30 @@
-module.exports = (req, res) => {
-  res.send("");
-};
-module.exports = (req, res) => {
-  res.send("");
+const DB = require("../utils/db");
+const generateToken = require("../utils/generateToken");
+
+module.exports = async (req, res, next) => {
+  const { username, db } = req.data;
+
+  try {
+    const token = generateToken();
+    const user = { username, token };
+
+    const newData = db.map(obj => {
+      if (username === obj.username) {
+        const item = { ...obj };
+
+        item.token = token;
+        return item;
+      }
+
+      return obj;
+    });
+
+    await DB.writeToDB(JSON.stringify(newData));
+
+    return res
+      .status(201)
+      .json({ message: `Login for "${username}" verified`, user });
+  } catch (err) {
+    return next(err);
+  }
 };
