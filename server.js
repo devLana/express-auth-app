3a13231444a4e6e3f3d1ff803b2b444954c1f4ff
ade_lana <lanadvictor@yahoo.co.uk>
2021-06-28 10:34:43 +0100

    apply cors & helmet middlewares

--- server.js
+++ server.js
@@ -1,13 +1,25 @@
 require("dotenv").config();
 
 const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
 
 const router = require("./routes");
 const error = require("./utils/Error");
 
 const app = express();
-const port = process.env.PORT || 3030;
 
+app.use(
+  cors({
+    origin: [
+      "http://localhost:9000",
+      "http://localhost:9090",
+      "https://basic-auth-api.herokuapp.com",
+    ],
+    methods: ["GET,POST"],
+  })
+);
+app.use(helmet());
 app.use(express.json());
 
 app.use("/api", router);
@@ -28,6 +40,6 @@ app.use((err, req, res, next) => {
     .json({ message: err.message, status: err.statusCode });
 });
 
-app.listen(port, () => {
-  console.log(`server running on port ${port}`);
+app.listen(process.env.PORT, () => {
+  console.log(`server running on port ${process.env.PORT}`);
 });
