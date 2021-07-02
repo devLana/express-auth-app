19e21f42905812d7d5f8285e82f10ca2e7890c85
ade_lana <lanadvictor@yahoo.co.uk>
2021-07-02 13:15:28 +0100

    remove unneccesary describe blocks

--- utils/getId/getId.test.js
+++ utils/getId/getId.test.js
@@ -1,18 +1,16 @@
 const getId = require("./index");
 
-describe("Generate new id", () => {
-  test("generates id from max id", () => {
-    const arr = [
-      { id: 1, username: "Jack", password: "1234abc" },
-      { id: 2, username: "Jane", password: "abc1234" },
-    ];
-    const id = getId(arr);
+test("generates id from max id", () => {
+  const arr = [
+    { id: 1, username: "Jack", password: "1234abc" },
+    { id: 2, username: "Jane", password: "abc1234" },
+  ];
+  const id = getId(arr);
 
-    expect(id).toBe(3);
+  expect(id).toBe(3);
 
-    const newArr = arr.filter(obj => obj.id !== 1);
-    const newId = getId(newArr);
+  const newArr = arr.filter(obj => obj.id !== 1);
+  const newId = getId(newArr);
 
-    expect(newId).toBe(3);
-  });
+  expect(newId).toBe(3);
 });
