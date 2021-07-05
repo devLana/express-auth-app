781cb7af113c22741544dde44367475e7a011af4
ade_lana <lanadvictor@yahoo.co.uk>
2021-07-05 15:20:58 +0100

    refactored checkToken tests

--- tests/checkToken.test.js
+++ tests/checkToken.test.js
@@ -1,66 +1,51 @@
 const supertest = require("supertest");
 const server = require("../server");
-const DB = require("../utils/db");
+const resetDB = require("./helper");
 
 const request = supertest(server);
 const message = "Invalid token. Logging user out";
 
 afterAll(async () => {
-  const data = [{ id: 1, username: "Jack", password: "1234abc" }];
-  await DB.writeToDB(JSON.stringify(data));
+  resetDB();
 });
 
+const testRequest = (token, status, errMessage) => {
+  return request
+    .get(`/api/verify-token/${token}`)
+    .expect("Content-Type", /application\/json/)
+    .then(response => {
+      expect(response.body.message).toBe(errMessage);
+      expect(response.body.status).toBe(status);
+    });
+};
+
 describe("GET /api/verify-token/:token", () => {
-  // test("token verification fails on empty token string", () => {
-  //   const token = " ";
-  //   return request
-  //     .get(`/api/verify-token/${token}`)
-  //     .expect("Content-Type", /application\/json/)
-  //     .expect(403)
-  //     .then(response => {
-  //       expect(response.body.message).toBe(message);
-  //       expect(response.body.status).toBe(403);
-  //     });
-  // });
+  test("token verification fails on empty token string", () => {
+    const token = "";
+    return testRequest(token, 404, "Resource not found");
+  });
 
   test("token verification fails on token string with invalid length", () => {
     const token = "Nj1n9E3yBJRw30HGXfWvTd6H4N";
-    return request
-      .get(`/api/verify-token/${token}`)
-      .expect("Content-Type", /application\/json/)
-      .expect(403)
-      .then(response => {
-        expect(response.body.message).toBe(message);
-        expect(response.body.status).toBe(403);
-      });
+    return testRequest(token, 403, message);
   });
 
   test("token verification fails on unknown but valid token string", () => {
     const token = "Nj61n9E3yBJRw38i30x672fI21e10HGXfWvT1801d6H4N";
-    return request
-      .get(`/api/verify-token/${token}`)
-      .expect("Content-Type", /application\/json/)
-      .expect(403)
-      .then(response => {
-        expect(response.body.message).toBe(message);
-        expect(response.body.status).toBe(403);
-      });
+    return testRequest(token, 403, message);
   });
 
-  test("token verification success on valid token string", () => {
+  test("token verification is successful on valid token string", () => {
     const body = { username: "Jack", password: "1234abc" };
     return request
       .post("/api/signin")
       .send(body)
       .then(response => {
-        return request
-          .get(`/api/verify-token/${response.body.user.token}`)
-          .expect("Content-Type", /application\/json/)
-          .expect(200)
-          .then(data => {
-            expect(data.body.message).toBe("Token verified. User is logged in");
-            expect(data.body.status).toBe(200);
-          });
+        return testRequest(
+          response.body.user.token,
+          200,
+          "Token verified. User is logged in"
+        );
       });
   });
 });
